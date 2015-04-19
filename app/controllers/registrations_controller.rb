83d14f1847a3061c0d0291f4b2603b1056eb6314
Wendy Yang <wendy.y.w.yang@gmail.com>
2015-04-20 00:24:53 +0800

    Added additional infos to users

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,7 +7,7 @@ def sign_up_params
 end
 
 def account_update_params
-	params.require(:user).permit(:email, :password, :password_confirmation, :username, :current_password)
+	params.require(:user).permit(:email, :password, :password_confirmation, :username, :current_password, :age, :gender, :bio)
 end
 
 end
