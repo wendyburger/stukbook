463edb403b50f53dda605376037d28fa162d14da
Wendy Yang <wendy.y.w.yang@gmail.com>
2015-04-19 23:51:30 +0800

    Added a user profile

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  
+  before_action :set_user, only:[:show] 
+
+  def show
+  end
+
+  private
+
+  def set_user
+  	@user = User.find_by(username: params[:id])
+  end
+
+end
