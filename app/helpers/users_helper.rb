227ae77884c9798f97778f7360789448b5f66aa5
Wendy Yang <wendy.y.w.yang@gmail.com>
2015-04-21 23:37:03 +0800

    Added friendship status detection

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+	def action_button(user)
+		case current_user.friendship_status(user) 
+		when "friends"
+			"Remove friendship"
+		when "pending"
+			"Cancel request"
+		when "requested"
+			"Accept or Deny"
+		when "not_friends"
+			"Added as a friend"			
+		end
+	end
 end
