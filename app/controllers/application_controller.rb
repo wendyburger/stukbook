548bdc1ad91ee343748bd0360ff72619849a29e8
Wendy Yang <wendy.y.w.yang@gmail.com>
2015-04-24 01:13:41 +0800

    Final touches before production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
+	helper_method :request_count
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+	def request_count	
+		current_user.pending_friendship_request_from.map(&:user).size
+	end
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+ 
   def home
+  	if current_user
+  		redirect_to activities_path
+  	end
   end
+
 end
